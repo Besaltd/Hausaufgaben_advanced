90a9c79b97e3a765aa1f2b2b94bf6f88ec53d02c
Ruslan <mellow.neliph@gmail.com>
2026-04-22 12:39:59 +0200

    Hausaufgabe_3.py wurde bearbeitet

--- Hausaufgabe_3.py
+++ Hausaufgabe_3.py
@@ -1,68 +1,22 @@
 import sqlalchemy
-from sqlalchemy import (
-    create_engine, Integer, String, Numeric, Boolean, ForeignKey, inspect, text
-)
-from sqlalchemy.orm import (
-    DeclarativeBase, Mapped, mapped_column,
-    relationship, sessionmaker
-)
+from sqlalchemy import Column, ForeignKey, Table, DECIMAL, Boolean, String, Integer
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, declarative_base, relationship
 
-
-engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=False)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
-class Base(DeclarativeBase):
-    pass
-
+Base = declarative_base()
 
 class Product(Base):
-    __tablename__ = 'products'
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    price = Column(DECIMAL(10, 2))
+    in_stock = Column(Boolean, default=True)
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
-    price: Mapped[float] = mapped_column(Numeric(10, 2))
-    in_stock: Mapped[bool] = mapped_column(Boolean, default=True)
-
-    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    category_id = Column(Integer, ForeignKey('category_id'))
 
 class Category(Base):
-    __tablename__ = 'categories'
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
-    description: Mapped[str] = mapped_column(String(255))
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(String(255))
 
     products = relationship('Product', backref='category')
-
-
-Base.metadata.create_all(engine)
-
-# cat = Category(name='Cars', description='All types of cars')
-#
-# mers = Product(name='Mercedes', price=50000, in_stock=True, category=cat)
-# bmw = Product(name='BWM', price=35000, in_stock=False, category=cat)
-# audi = Product(name='Audi', price=45000, in_stock=True, category=cat)
-#
-# session.add(cat)
-# session.add_all([mers, bmw, audi])
-# session.commit()
-#
-# with engine.connect() as connect:
-#     print(f"Products: ")
-#     rows = connect.execute(text("SELECT * FROM products")).fetchall()
-#     for _ in rows:
-#         print(_)
-#
-#     print(f"\nCategories: ")
-#     rows = connect.execute(text("SELECT * FROM categories")).fetchall()
-#     for _ in rows:
-#         print(_)
-#
-# insp = inspect(engine)
-#
-# print(insp.get_table_names())
-# print(insp.get_columns('products'))
-# print(insp.get_columns('categories'))
-
